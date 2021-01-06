37ba8aad6f847e1bfaf200b1e383add650dddf07
abelzhu <abel_zhu@msn.com>
2021-01-06 16:17:54 +0800

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,4 +2,4 @@
 
 ################################################################################
 
-docker run -p 8888:8888 -p49053:49053 -v "$PWD":/home/sliceruser/work --rm -ti lassoan/slicer-notebook:2020-05-15-89b6bb5
+docker run -p 18888:8888 -p49053:49053 -v "$PWD":/home/sliceruser/work --rm -ti lassoan/slicer-notebook:2020-05-15-89b6bb5
